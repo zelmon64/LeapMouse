70be83dc83ddb08d5ca19e690e72be621daa4537
zelmon64 <zelmon64@gmail.com>
2017-12-22 16:17:15 +0000

    Changed visual feedback to an arc at the top of the screen
    The ring around the mouse cursor was using too much resources. This
    stationary indicator can be more efficiently refreshed / cleaned.

--- LeapMouse/LeapListener.cs
+++ LeapMouse/LeapListener.cs
@@ -32,20 +32,12 @@ class LeapListener
         }
     }
 
-    public float CursorXPos;
-    public float CursorYPos;
-    public bool MouseOn;
-    public bool KbOn;
     public void OnInit(Controller controller)
     {
         IntPtr h = Process.GetCurrentProcess().MainWindowHandle;
-        ShowWindow(h, 0);
+        //ShowWindow(h, 0);
         //controller.EnableGesture(Gesture.GestureType.TYPESWIPE);
         SafeWriteLine("Initialized");
-        CursorXPos = 0;
-        CursorYPos = 0;
-        MouseOn = false;
-        KbOn = false;
     }
 
     public void OnConnect(object sender, DeviceEventArgs args)
@@ -121,116 +113,155 @@ class LeapListener
     {
         prevFrame = currentFrame;
         currentFrame = args.frame;
-        //int appWidth = Screen.PrimaryScreen.WorkingArea.Width;
-        //int appHeight = Screen.PrimaryScreen.WorkingArea.Height;
-
-        InteractionBox iBox = currentFrame.InteractionBox;
-        //if (currentFrame.Hands.Count > 0)
         currentTime = currentFrame.Timestamp;
         changeTime = currentTime - prevTime;
-        /*
-        Console.WriteLine("Frame id: {0}, timestamp: {1}, hands: {2}, changeTime: {3}",
-            currentFrame.Id, currentFrame.Timestamp, currentFrame.Hands.Count, changeTime);
-        */
-        if (currentFrame.Hands.Count > 0) // && prevFrame.Hands.Count > 0)
+
+        if (changeTime > 3e4) // time in microseconds (1e-6)
         {
-            Hand hand = currentFrame.Hands[0];
-            Finger finger = hand.Fingers[1];
+            InteractionBox iBox = currentFrame.InteractionBox;
 
-            Leap.Vector leapPoint = finger.TipPosition; //.StabilizedTipPosition; //
-            Leap.Vector normalizedPoint = iBox.NormalizePoint(leapPoint, false);
-            currentZ = normalizedPoint.z;
-
-            if (changeTime > 3e3 & currentZ < 1.2) // & currentZ > 0.3
+            if (currentFrame.Hands.Count > 0) // && prevFrame.Hands.Count > 0)
             {
-                drawSkip++;
+                Hand hand = currentFrame.Hands[0];
+                Finger finger = hand.Fingers[1];
 
-                //currentThumb = hand.Fingers[0].IsExtended;
+                Leap.Vector leapPoint = finger.TipPosition; //.StabilizedTipPosition; //
+                Leap.Vector normalizedPoint = iBox.NormalizePoint(leapPoint, false);
+                currentZ = normalizedPoint.z;
 
-                float CursorXPos = normalizedPoint.x; // * 65535; // appWidth;
-                float CursorYPos = (1 - normalizedPoint.y); // * 65535; // appHeight;
-                int CursorXPosMouse = (int)(CursorXPos * 65535);
-                int CursorYPosMouse = (int)(CursorYPos * 65535);
-                int CursorXPosScreen = (int)(CursorXPos * 1920); // Screen.PrimaryScreen.WorkingArea.Width);
-                int CursorYPosScreen = (int)(CursorYPos * 1080); // Screen.PrimaryScreen.WorkingArea.Height);
-
-                // Move the mouse.
-                //SetCursorPos((int)CursorXPos, (int)CursorYPos);
-                mouse_event(
-                    (MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE),
-                    CursorXPosMouse, CursorYPosMouse, 0, 0);
-
-                if (drawSkip > 10)
+                if (currentZ < 1.5) // & currentZ > 0.3
                 {
-                    drawSkip = 1;
-                    //Point pt = Cursor.Position; // Get the mouse cursor in screen coordinates
-                    /*
-                    using (Graphics g = Graphics.FromHwnd(IntPtr.Zero))
+                    drawSkip++;
+
+                    //currentThumb = hand.Fingers[0].IsExtended;
+
+                    float CursorXPos = normalizedPoint.x; // * 65535; // appWidth;
+                    float CursorYPos = (1 - normalizedPoint.y); // * 65535; // appHeight;
+                    int CursorXPosMouse = (int)(CursorXPos * 65535);
+                    int CursorYPosMouse = (int)(CursorYPos * 65535);
+                    int CursorXPosScreen = (int)(CursorXPos * 1920); // Screen.PrimaryScreen.WorkingArea.Width);
+                    int CursorYPosScreen = (int)(CursorYPos * 1080); // Screen.PrimaryScreen.WorkingArea.Height);
+
+                    // Move the mouse.
+                    //SetCursorPos((int)CursorXPos, (int)CursorYPos);
+                    mouse_event(
+                        (MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE),
+                        CursorXPosMouse, CursorYPosMouse, 0, 0);
+
+                    if (drawSkip > 3) // & currentZ < 1 & currentZ > 0) //(false) //
                     {
-                        g.DrawEllipse(Pens.Black, CursorXPos - 10, CursorYPos - 10, 20, 20);
+                        drawSkip = 1;
+                        if (currentZ > 1)
+                            currentZ = 1;
+                        else if (currentZ < 0)
+                            currentZ = 0;
+                        IntPtr desktopPtr = GetDC(IntPtr.Zero);
+                        Graphics g = Graphics.FromHdc(desktopPtr);
+                        float penThickness = 8;
+                        Pen arcPen = new Pen(Color.Transparent, penThickness);
+                        int retSize = 300;
+                        Rectangle arcRet = new Rectangle((1920 - retSize) / 2, 50, retSize, retSize); // ((1920- retSize) / 2, (1080- retSize) / 2, retSize, retSize); // (50, 50, 1870, 1030);
+                        float startAng; // = 180;
+                        float sweepAng; // = 180;
+
+                        float angMult = 20;
+                        if (currentZ > 0.5)
+                        {
+                            sweepAng = (float)(angMult * 2 * (currentZ - 0.5));
+                            startAng = 270 - sweepAng;
+                            if (sweepAng != angMult)
+                                try
+                                {
+                                    g.DrawArc(arcPen, arcRet, 270 - angMult, angMult - sweepAng);
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine("Draw Error in frame: " + currentFrame + ":\n"+ e);
+                                }
+                            g.DrawArc(arcPen, arcRet, 270, angMult);
+                            arcPen.Dispose();
+                            arcPen = new Pen(Color.LawnGreen, penThickness);
+                        }
+                        else
+                        {
+                            startAng = 270;
+                            sweepAng = (float)(angMult * 2 * (0.5 - currentZ));
+                            if (sweepAng != angMult)
+                                try
+                                {
+                                    g.DrawArc(arcPen, arcRet, angMult - 90, sweepAng - angMult);
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine("Draw Error in frame: " + currentFrame + ":\n" + e);
+                                }
+                            g.DrawArc(arcPen, arcRet, 270, -angMult);
+                            arcPen.Dispose();
+                            arcPen = new Pen(Color.Red, penThickness);
+                        }
+                        if (sweepAng > 0)
+                            try
+                            {
+                                g.DrawArc(arcPen, arcRet, startAng, sweepAng);
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine("Draw Error in frame: " + currentFrame + ":\n" + e);
+                            }
+                        //g.DrawArc(new Pen(Color.Transparent, 5), acrRet, (startAng + sweepAng), (360 - sweepAng));
+                        /*
+                        // Create pens.
+                        Pen blackPen = new Pen(Color.Black, 5);
+                        Pen redPen = new Pen(Color.Red, 5);
+                        // Clear screen
+                        g.DrawRectangle(Pens.Transparent, new Rectangle(0, 0, 1920, 1080));
+                        int sizeMult = 150;
+                        if (currentZ > 0.5)
+                            g.DrawEllipse(blackPen, new Rectangle(CursorXPosScreen - (int)(sizeMult * (currentZ - 0.5)), CursorYPosScreen - (int)(sizeMult * (currentZ - 0.5)), (int)(2 * sizeMult * (currentZ - 0.5)), (int)(2 * sizeMult * (currentZ - 0.5))));
+                        else
+                            g.DrawEllipse(redPen, new Rectangle(CursorXPosScreen - (int)(sizeMult * (0.5 - currentZ)), CursorYPosScreen - (int)(sizeMult * (0.5 - currentZ)), (int)(2 * sizeMult * (0.5 - currentZ)), (int)(2 * sizeMult * (0.5 - currentZ))));
+                        //*/
+                        g.Dispose();
+                        arcPen.Dispose();
+
+                        ReleaseDC(IntPtr.Zero, desktopPtr);
                     }
-                    */
-                    IntPtr desktopPtr = GetDC(IntPtr.Zero);
-                    Graphics g = Graphics.FromHdc(desktopPtr);
 
-                    //SolidBrush b = new SolidBrush(Color.Black);
-                    // Create pen.
-                    Pen blackPen = new Pen(Color.Black, 5);
-                    Pen redPen = new Pen(Color.Red, 5);
-                    //g.FillRectangle(b, new Rectangle(0, 0, 1920, 1080));
-                    g.DrawRectangle(Pens.Transparent, new Rectangle(0, 0, 1920, 1080));
-                    //g.FillEllipse(b, new Rectangle(CursorXPosScreen - 10, CursorYPosScreen - 10, CursorXPosScreen + 10, CursorYPosScreen + 10));
-                    //g.DrawEllipse(Pens.Black, new Rectangle(CursorXPosScreen - 10, CursorYPosScreen - 10, 20, 20));
-                    int sizeMult = 100;
-                    if (currentZ > 0.5)
-                        g.DrawEllipse(blackPen, new Rectangle(CursorXPosScreen - (int)(sizeMult * (currentZ - 0.5)), CursorYPosScreen - (int)(sizeMult * (currentZ - 0.5)), (int)(2 * sizeMult * (currentZ - 0.5)), (int)(2 * sizeMult * (currentZ - 0.5))));
-                    //g.FillEllipse(b, new Rectangle(CursorXPosScreen - (int)(sizeMult * (currentZ - 0.5)), CursorYPosScreen - (int)(sizeMult * (currentZ - 0.5)), (int)(2 * sizeMult * (currentZ - 0.5)), (int)(2 * sizeMult * (currentZ - 0.5))));
-                    else
-                        g.DrawEllipse(redPen, new Rectangle(CursorXPosScreen - (int)(sizeMult * (0.5 - currentZ)), CursorYPosScreen - (int)(sizeMult * (0.5 - currentZ)), (int)(2 * sizeMult * (0.5 - currentZ)), (int)(2 * sizeMult * (0.5 - currentZ))));
+                    // Left click
+                    //*
+                    //if (!previousThumb & currentThumb)
+                    if (previousZ > 0.5 & currentZ < 0.5)
+                    {
+                        mouse_event(MOUSEEVENTF_LEFTDOWN, (int)CursorXPos, (int)CursorYPos, 0, 0);
+                        mouseDown = true;
+                    }
+                    //if (previousThumb & !currentThumb)
+                    if (previousZ < 0.5 & currentZ > 0.5)
+                    {
+                        mouse_event(MOUSEEVENTF_LEFTUP, (int)CursorXPos, (int)CursorYPos, 0, 0);
+                        mouseDown = false;
+                    }//*/
 
-                    g.Dispose();
-                    ReleaseDC(IntPtr.Zero, desktopPtr);
+                    //Console.Write("X: " + normalizedPoint.x + ", Y: " + normalizedPoint.y + ", Z: " + normalizedPoint.z + "\n");
+
+                    previousZ = currentZ;
+                    prevTime = currentTime;
+                    //previousThumb = currentThumb;
                 }
-
-                // Left click
-                //if (!previousThumb & currentThumb)
-                if (previousZ > 0.5 & currentZ < 0.5)
-                {
-                    mouse_event(MOUSEEVENTF_LEFTDOWN, (int)CursorXPos, (int)CursorYPos, 0, 0);
-                    mouseDown = true;
-                }
-                //if (previousThumb & !currentThumb)
-                if (previousZ < 0.5 & currentZ > 0.5)
-                {
-                    mouse_event(MOUSEEVENTF_LEFTUP, (int)CursorXPos, (int)CursorYPos, 0, 0);
-                    mouseDown = false;
-                }
-
-                //mouse_event(0x8000, (int)CursorXPos, (int)CursorYPos, 0, 0);
-                //mouse_event(0x0100, (int)CursorXPos, (int)CursorYPos, 0x0001, 0);
-                //mouse_event(0x0100, (int)CursorXPos, (int)CursorYPos, 0x0002, 0);
-                //The z-coordinate is not used
-                //Console.Write("X: " + CursorXPos + ", Y: " + CursorYPos + "\n");
-                //Console.Write("X: " + normalizedPoint.x + ", Y: " + normalizedPoint.y + "\n");
-                //Console.Write("X: " + normalizedPoint.x + ", Y: " + normalizedPoint.y + ", Z: " + normalizedPoint.z + "\n");
-                //Console.Write("X: " + leapPoint.x + ", Y: " + leapPoint.y + "\n");
-
-                previousZ = currentZ;
-                prevTime = currentTime;
-                //previousThumb = currentThumb;
             }
-        }
-        else
-        {
-            //Console.Write("No hands found\n");
-            if (mouseDown)
+            else
             {
-                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
-                mouseDown = false;
+                //Console.Write("No hands found\n");
+                //*
+                if (mouseDown)
+                {
+                    mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                    mouseDown = false;
+                }//*/
+                currentZ = 1;
+                previousZ = 1;
+                //currentThumb = false;
             }
-            currentZ = 1;
-            previousZ = 1;
-            //currentThumb = false;
         }
     }
 }
